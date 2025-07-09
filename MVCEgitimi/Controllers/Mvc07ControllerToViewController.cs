a37f5ff0c271c42f4979c559ee9db009efef6bd1
Bashkal <mahmutsamibaska@gmail.com>
2025-07-09 12:28:46 +0300

    9.7.2025 öğle arasından önce

---
+++ MVCEgitimi/Controllers/Mvc07ControllerToViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitimi.Controllers
+{
+    public class Mvc07ControllerToViewController : Controller
+    {
+        // GET: Mvc07ControllerToView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
