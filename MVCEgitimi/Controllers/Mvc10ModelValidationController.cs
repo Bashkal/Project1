6e4546c40dec6be523f3c79bc5e9e047a644ec60
Bashkal <mahmutsamibaska@gmail.com>
2025-07-11 17:42:59 +0300

    11.007.2025 değişiklikler Dmakina

--- MVCEgitimi/Controllers/Mvc10ModelValidationController.cs
+++ MVCEgitimi/Controllers/Mvc10ModelValidationController.cs
@@ -1,6 +1,7 @@
 ﻿using MVCEgitimi.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,14 +22,61 @@ namespace MVCEgitimi.Controllers
         [HttpPost]
 
         public ActionResult YeniUye(Uye uye)
-        { if(ModelState.IsValid)
-            ViewBag.UyeBilgi = $"Ad: {uye.Ad}<br/> Soyad: {uye.Soyad}<br/> Eposta: {uye.Eposta}<br/> Kullanıcı Adı: {uye.KullaniciAdi}<br/> TC Kimlik No: {uye.TcKimlikNo}<br/> Telefon: {uye.Telefon}<br/> Şifre: {uye.Sifre}<br/> Doğum Tarihi: {uye.DogumTarihi.ToString("dd.MM.yyyy")}";
+        {
+            if (ModelState.IsValid)
+                ViewBag.UyeBilgi = $"Ad: {uye.Ad}<br/> Soyad: {uye.Soyad}<br/> Eposta: {uye.Eposta}<br/> Kullanıcı Adı: {uye.KullaniciAdi}<br/> TC Kimlik No: {uye.TcKimlikNo}<br/> Telefon: {uye.Telefon}<br/> Şifre: {uye.Sifre}<br/> Doğum Tarihi: {uye.DogumTarihi.ToString("dd.MM.yyyy")}";
             else
             {
                 ViewBag.UyeBilgi = "Model Validasyonu Hatası";
                 return View(uye);
             }
             return View();
+        }
+        public ActionResult UyeDuzenle(int id)
+        {
+            
+            return View();
+        }
+        public ActionResult UyeSil()
+        {
+            var uyelistesi = new List<Uye>()
+            {
+                new Uye()
+                {
+                    Id=1,
+                    Ad="Ali",
+                    Soyad="Yılmaz",
+                    Eposta=""
+            },
+                new Uye()
+                {
+                    Id=2,
+                    Ad="Ayşe",
+                    Soyad="Demir",
+                    Eposta=""
+            }};
+
+            return View(uyelistesi);
+        }
+        public ActionResult UyeListele()
+        {
+            var uyelistesi = new List<Uye>()
+            {
+                new Uye()
+                {
+                    Id=1,
+                    Ad="Ali",
+                    Soyad="Yılmaz",
+                    Eposta=""
+            },
+                new Uye()
+                {
+                    Id=2,
+                    Ad="Ayşe",
+                    Soyad="Demir",
+                    Eposta=""
+            }};
+            return View(uyelistesi);
         }
     }
 }
