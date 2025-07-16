236aca402674972f9d1280092f4c032843c2db79
Bashkal <mahmutsamibaska@gmail.com>
2025-07-16 17:28:05 +0300

    16.07.2025 DM Değişiklikler

---
+++ MVCEgitimi/Controllers/Mvc11SessionCookieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitimi.Controllers
+{
+    public class Mvc11SessionCookieController : Controller
+    {
+        // GET: Mvc11SessionCookie
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string text)
+        {
+            //session değer atama 
+            Session["SessionText"] = text;
+            //Dİğer method
+            //Session.Add("SessionText", text);
+            return View();
+        }
+        public ActionResult Sessions()
+        {
+            if (true)
+            {
+            }
+            return View();
+        }
+    }
+}
