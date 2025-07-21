263fa0673cf011dc784a27a2a9b35a853b8ec710
Bashkal <mahmutsamibaska@gmail.com>
2025-07-21 10:39:55 +0300

    21.07.2025 Sabah yapılan değişiklikler ve taslak projenin açılmasıyla oluşan değişiklikler

--- MVCEgitimi/Controllers/Mvc13ViewResultController.cs
+++ MVCEgitimi/Controllers/Mvc13ViewResultController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,51 @@ namespace MVCEgitimi.Controllers
         public RedirectResult Index2()
         {
             return Redirect("/Home/Index") ;
+        }
+        public RedirectToRouteResult Index3()
+        {
+            return RedirectToRoute("Default",new{ controller = "Home", action = "Index" });
+        }
+        public PartialViewResult KategorileriPartialIleGetir()
+        {
+            return PartialView("_KategorilerPartial");
+        }
+        public PartialViewResult KategorileriModelIleGetir()
+        {
+            List<string> kategoriler = new List<string>(){
+            "Bilgisayarlar",
+            "Monitörler",
+            "Kalvyeler",
+            "Mouselar"};
+
+            return PartialView("_KategorilerPartial2",kategoriler);
+        }
+        public FileResult PDFDosyaIndir()
+        {
+            string dosyaYolu = Server.MapPath("~/wwwroot/IFS Cloud Course Catalogue for Customers.pdf");
+            return new FilePathResult(dosyaYolu, "application/pdf");
+        }
+        public FileStreamResult MetinDosyaIndir()
+        {
+            string dosyaYolu = Server.MapPath("~/wwwroot/notlar.txt");
+            var stream = new System.IO.FileStream(dosyaYolu, System.IO.FileMode.Open);
+            return new FileStreamResult(stream, "text/plain")
+            {
+                FileDownloadName = "Notes.txt"
+            };
+        }
+        public FileStreamResult MetinDosyasiIndir()
+        {
+            string metin="Merhaba, bu bir metin dosyasıdır.";
+            MemoryStream memoryStream = new MemoryStream();
+            byte[] bytes= System.Text.Encoding.UTF8.GetBytes(metin);
+            memoryStream.Write(bytes, 0, bytes.Length);
+            memoryStream.Position = 0;
+            FileStreamResult result = new FileStreamResult(memoryStream, "text/plain");
+            result.FileDownloadName = "metin.txt";
+            return result;
+            
+
         }
     }
 }
