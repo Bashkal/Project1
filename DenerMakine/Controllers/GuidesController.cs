0fc1f3c7dcb902d05b6ebc08fe34e30a677418b8
Bashkal <mahmutsamibaska@gmail.com>
2025-08-01 16:29:27 +0300

    video without chapters

---
+++ DenerMakine/Controllers/GuidesController.cs
@@ -0,0 +1,30 @@
+﻿using DenerMakine.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DenerMakine.Controllers
+{
+    public class GuidesController : Controller
+    {
+        private readonly DataBaseContext _context;
+        public GuidesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> IndexAsync()
+        {
+            var model =await _context.Guides.Include(g=>g.Department).ToListAsync();
+            return View(model);
+        }
+        public async Task <IActionResult> DetailsAsync(int id)
+        {
+            var model =await _context.Guides.FindAsync(id);
+            return View(model);
+        }
+        public async Task<IActionResult> SearchAsync(string? search)
+        {
+            var model = await _context.Guides.Include(g => g.Department).Where(p => p.Name.Contains(search)||p.Description.Contains(search)||p.Department.Name.Contains(search)).ToListAsync();
+            return View(model);
+        }
+    }
+}
