63d1c2376b29790d734b01e3d79f6864d8f5a480
Bashkal <mahmutsamibaskal@gmail.com>
2025-10-21 10:22:29 +0300

    Delete Deneme_Sitesi directory

--- Deneme_Sitesi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Deneme_Sitesi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
