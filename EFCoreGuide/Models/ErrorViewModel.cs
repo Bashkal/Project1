5c9fa04e852a1c8587e5684b303b26600d9528b8
Bashkal <mahmutsamibaskal@gmail.com>
2025-10-21 10:22:57 +0300

    Delete EFCoreGuide directory

--- EFCoreGuide/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EFCoreGuide.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
